6c2ae1981431e3992862ec1076564c67fc505733
SiHyun-Lee113 <tlgus0113@gmail.com>
2023-02-06 15:09:27 +0900

    .

---
+++ src/test/java/com/example/awswithspringproject/AwSwithSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awswithspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwSwithSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
