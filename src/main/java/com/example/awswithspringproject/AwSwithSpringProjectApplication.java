f67d61182493a38fefa177a6758cbd16889c3cfc
SiHyun-Lee113 <tlgus0113@gmail.com>
2023-02-08 16:15:18 +0900

    main 메서드 하나 주석처리

--- src/main/java/com/example/awswithspringproject/AwSwithSpringProjectApplication.java
+++ src/main/java/com/example/awswithspringproject/AwSwithSpringProjectApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AwSwithSpringProjectApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(AwSwithSpringProjectApplication.class, args);
-    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(AwSwithSpringProjectApplication.class, args);
+//    }
 
 }
